fa09f2fa87b14a9de9802867442ea1621c6c5aaf
Rishabh Desai <desai.rishab@gmail.com>
2021-04-20 00:59:57 +0530

    state management, hidden form field

---
+++ StateManagement_HiddenFormField/src/main/java/pages/FirstServlet.java
@@ -0,0 +1,43 @@
+package pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/first"})
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String name = request.getParameter("name");
+		
+	
+		response.setContentType("text/html");
+		try(PrintWriter out = response.getWriter()){
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>first servlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+			out.println("<form action='second'>");
+				
+			out.println("<h1>First servlet, Welcome "+name+" </h1>");
+			
+			out.println("<input type='hidden' name='firstCookie' value='"+name+"'/>");
+			out.println("<input type='submit' value='submit'>");
+			out.println("</form>");
+			out.println("</body>");
+			out.println("</html>");
+		}
+	
+	}
+
+}
