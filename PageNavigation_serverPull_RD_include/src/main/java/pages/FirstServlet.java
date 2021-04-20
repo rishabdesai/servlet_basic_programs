0af6a1221146d3cc7e798fdae57379391ce68212
Rishabh Desai <desai.rishab@gmail.com>
2021-04-20 12:51:31 +0530

    include() scenario

---
+++ PageNavigation_serverPull_RD_include/src/main/java/pages/FirstServlet.java
@@ -0,0 +1,60 @@
+package pages;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(urlPatterns = { "/first" })
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String name = request.getParameter("IndexPageUserName");
+		HttpSession session = request.getSession();
+		session.setAttribute("SessionNameKey", name);
+
+		ServletContext application = this.getServletContext();
+		application.setAttribute("applicationNameKey", name);
+	
+		try(PrintWriter out = response.getWriter()){
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<title>second servlet</title>");
+			out.println("</head>");
+			out.println("<body>");
+			
+			String url = response.encodeURL("third");
+			out.println("<form action='"+url+"'>");
+			out.println("<h1>first servlet, Welcome "+name+" </h1>");
+			out.println("<input type='submit' value='submit'>");
+			out.println("</form");
+			out.println("</body>");
+			out.println("</html>");
+			
+			
+			//page navigation -server pull - request dispatcher - include() 
+			String path = response.encodeURL("second"); //note: '/' is optional
+			  RequestDispatcher rd = request.getRequestDispatcher(path); 
+			  rd.include(request, response);
+		}
+		
+		
+		
+	
+	}
+	
+	
+	
+}
